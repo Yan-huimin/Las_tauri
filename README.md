8ed7bafac2bdd0930bf32776ba11ee79f20477da
yhm <2234489774@qq.com>
2026-02-21 21:06:38 +0800

    first commit

---
+++ README.md
@@ -0,0 +1,1 @@
+:star: A tauri demo
---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import '@/App'
+
+function App() {
+
+  return (
+    <>
+      <div className='fixed w-full h-full flex items-center justify-center bg-black'>
+        <h1 className='bg-blue-500 text-white p-4 rounded-lg'>hello World</h1>
+      </div>
+    </>
+  )
+}
+
+export default App
